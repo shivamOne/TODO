d9a7dd7281158953e99f987243da9f77cce7730e
shivamOne <43596489+shivamOne@users.noreply.github.com>
2022-06-26 21:03:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { Input } from "./components/Input";
+
+function App() {
+
+  return (
+    <div className="cover">
+      <h3>TODO - APP</h3>
+      <div className="form-and-todo-box">
+        <Input/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
